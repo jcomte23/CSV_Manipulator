eb4700903c441a9ae8aaee1dfac3f6e0cc3bb8fa
Javier Combita Tellez <javiercombita2014@gmail.com>
2023-10-20 13:34:42 -0500

    console view for errors was created

--- Console_views/views.py
+++ Console_views/views.py
@@ -1,14 +1,27 @@
-INTERNAL_SPACING = 10
+INTERNAL_SPACING_TITLE = 18
+INTERNAL_SPACING_ERROR = 5
 
 
 def show_title(principal_title):
-    # principal_title = "Welcome to the CSV manipulator"
     title_size = len(principal_title)
 
-    interface_size = title_size + (INTERNAL_SPACING * 2)
+    interface_size = title_size + (INTERNAL_SPACING_TITLE * 2)
     print(interface_size)
 
     print()
     print("=" * interface_size)
-    print("|" + " " * (INTERNAL_SPACING - 1) + principal_title + " " * (INTERNAL_SPACING - 1) + "|")
+    print("|" + " " * (INTERNAL_SPACING_TITLE - 1) + principal_title + " " * (INTERNAL_SPACING_TITLE - 1) + "|")
     print("=" * interface_size)
+
+
+def show_error(error):
+    principal_title = f"ERROR => {error}"
+    title_size = len(principal_title)
+
+    interface_size = title_size + (INTERNAL_SPACING_ERROR * 2)
+    print(interface_size)
+
+    print()
+    print("!" * interface_size)
+    print("|" + " " * (INTERNAL_SPACING_ERROR - 1) + principal_title + " " * (INTERNAL_SPACING_ERROR - 1) + "|")
+    print("!" * interface_size)
--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 import Console_views.views as view
 
 view.show_title("Welcome to the CSV manipulator")
+
+try:
+    number = int(input("Enter"))
+except ValueError as error:
+    view.show_error(error.args[0])
